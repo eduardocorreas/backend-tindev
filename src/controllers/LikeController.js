690543fbe915d36fbc9b0ebe6cef47c0ae26bd87
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2019-08-12 09:04:14 -0300

    first commit

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,29 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    try {
+      const loggedDev = await Dev.findById(user);
+      const targetDev = await Dev.findById(devId);
+
+      if (!targetDev) {
+        return res.status(400).json({ error: "User not exists" });
+      }
+
+      if (targetDev.likes.includes(loggedDev._id)) {
+        console.log("DEU MATCH");
+      }
+
+      loggedDev.likes.push(targetDev._id);
+
+      loggedDev.save();
+
+      return res.json(loggedDev);
+    } catch (err) {
+      return res.json({ error: err });
+    }
+  }
+};
